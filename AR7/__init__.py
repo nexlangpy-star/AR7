3a1105868a45499b00299e8cd1b792ccb6776029
nexlangpy-star <nexlangpy@gmail.com>
2026-07-23 17:49:07 +0300

    تحديث

--- AR7/__init__.py
+++ AR7/__init__.py
@@ -6,4 +6,4 @@ os.environ['LD_LIBRARY_PATH'] = lib_dir + ':' + os.environ.get('LD_LIBRARY_PATH'
 
 if lib_dir not in sys.path:
     sys.path.insert(0, lib_dir)
-__version__ = "1.3.2"
+__version__ = "1.3.3"
